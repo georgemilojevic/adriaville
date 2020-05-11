ec2d079470ba4a3e7d69a248c0993b3d291e4f55
Djordje Milojevic <georgemilojevic@gmail.com>
2020-05-11 21:43:40 +0200

    migrations

---
+++ database/migrations/2020_05_11_192326_header_slider.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class HeaderSlider extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('header_sliders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('title');
+            $table->text('description');
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('header_sliders');
+    }
+}
